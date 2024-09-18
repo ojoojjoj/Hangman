5b85ce0232402b97e2ae2129e15714b0cb631407
benjaminosterlund <osterlund_benjamin@hotmail.com>
2024-09-18 19:02:39 +0200

    Adding incomplete test CanConstructGameContent

---
+++ Hangman.Tests/GameContentTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hangman;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman.Tests
+{
+    [TestClass()]
+    public class GameContentTests
+    {
+        [TestMethod()]
+        public void CanConstructGameContent()
+        {
+            //Arrange
+            var randomWord = "LAMPA";
+            var displayRandomWord = new char[] { '_', '_', '_', '_', '_' };
+
+            //Act
+            var gameContent = new GameContent(randomWord, displayRandomWord);
+            
+            //Assert
+            Assert.Fail();
+        }
+    }
+}
